b8b41cae1bcdcf86fde23657c2c5dbc948ea38f4
Platiplus <quazarweb@gmail.com>
2019-11-23 13:48:08 -0300

    createGame method implemented

--- src/api/controllers/game-controller.js
+++ src/api/controllers/game-controller.js
@@ -1,6 +1,17 @@
+// DEPENDENCIES
+const Game = require('../models/game-model')
+const GameManager = require('../models/manager-model')
+
+// CONTROL VARS
+const manager = new GameManager()
+manager.watchOurGames()
+
 // CONTROLLERS
 const createGame = (request, response) => {
-  // @TODO CREATE GAME METHOD
+  const game = new Game()
+  game.start()
+  manager.watchThisGame(game)
+  return response.status(200).json({ id: game.id, firstPlayer: game.playerTurn })
 }
 
 const makeMove = (request, response) => {
